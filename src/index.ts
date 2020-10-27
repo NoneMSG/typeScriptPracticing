f141d30764b35b1bbb50515fe368c9619ea0a0a1
moon <princeps90@gmail.com>
2020-10-28 00:18:35 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+interface Human {
+    name : string;
+    age : number;
+    gender : string;
+}
+
+const person ={
+    name : "moon",
+    age : 31,
+    gender : 'male'
+};
+
+const sayHi = (person : Human):string =>{
+    return `Hello ${person.name}. you are ${person.age}, you are a ${person.gender}!!`;
+};
+    
+console.log(sayHi(person));
+ 
+export {};
+
